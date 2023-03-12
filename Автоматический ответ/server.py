5c093ee825066e6e6444cf9711a238cf118df6d5
PitchBlackMilk <m.denisov16.01@gmail.com>
2023-03-12 15:39:42 +0300

    Автоматический ответ

---
+++ Автоматический ответ/server.py
@@ -0,0 +1,34 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    param = {}
+    param['username'] = "Ученик Яндекс.Лицея"
+    param['title'] = 'Домашняя страница'
+    return render_template('index.html', **param)
+
+@app.route('/answer')
+@app.route('/auto_answer')
+def answer():
+    param = {}
+    param['surname'] = "Ученик Яндекс.Лицея"
+    param['name'] = "Ученик"
+    param['title'] = 'страница'
+    return render_template('auto_answer.html', title1=param['title'], item=param)
+
+@app.route('/index/<title>')
+def index1(title):
+    return render_template('base.html', title1=title)
+
+@app.route('/training/<prof>')
+def training(prof):
+    return render_template('training.html', title1='training', prof=prof)
+
+
+
+
+if __name__ == '__main__':
+    app.run(port=8080, host='127.0.0.1')
